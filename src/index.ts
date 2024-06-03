4e494943f48482370dde6776939b79e785fe8a9c
Arslan <muhammad.arslan.82jb@gmail.com>
2024-06-04 00:21:06 +0500

    messageHandler() implimented in Index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,40 @@
-import { server as WebSocketServer } from "websocket";
+import { server as WebSocketServer, connection } from "websocket";
 import http from "http";
+import {
+  IncomingMessage,
+  SupportedMessage,
+} from "./messages/incommingMessages";
+import {
+  OutgoingMessage,
+  SupportedMessage as OutgoingSupportedMessages,
+} from "./messages/outgoingMessages";
+import { UserManager } from "./UserManager";
+import { InMemoryStore } from "./store/InMemoryStore";
 
 const server = http.createServer(function (request: any, response: any) {
   console.log(new Date() + " Received request for " + request.url);
   response.writeHead(404);
   response.end();
 });
+
+const userManager = new UserManager();
+const store = new InMemoryStore();
+
 server.listen(8080, function () {
   console.log(new Date() + " Server is listening on port 8080");
 });
 
 const wsServer = new WebSocketServer({
   httpServer: server,
-  // You should not use autoAcceptConnections for production
-  // applications, as it defeats all standard cross-origin protection
-  // facilities built into the protocol and the browser.  You should
-  // *always* verify the connection's origin and decide whether or not
-  // to accept it.
-  autoAcceptConnections: false,
+  autoAcceptConnections: true,
 });
 
 function originIsAllowed(origin: any) {
-  // put logic here to detect whether the specified origin is allowed.
   return true;
 }
 
 wsServer.on("request", function (request) {
   if (!originIsAllowed(request.origin)) {
-    // Make sure we only accept requests from an allowed origin
     request.reject();
     console.log(
       new Date() + " Connection from origin " + request.origin + " rejected."
@@ -35,12 +42,13 @@ wsServer.on("request", function (request) {
     return;
   }
 
-  var connection = request.accept();
+  var connection = request.accept("echo-protocol", request.origin);
   console.log(new Date() + " Connection accepted.");
   connection.on("message", function (message) {
     if (message.type === "utf8") {
-      console.log("Received Message: " + message.utf8Data);
-      connection.sendUTF(message.utf8Data);
+      try {
+        messageHandler(connection, JSON.parse(message.utf8Data));
+      } catch (error) {}
     } else if (message.type === "binary") {
       console.log(
         "Received Binary Message of " + message.binaryData.length + " bytes"
@@ -54,3 +62,62 @@ wsServer.on("request", function (request) {
     );
   });
 });
+
+function messageHandler(ws: connection, message: IncomingMessage) {
+  if (message.type == SupportedMessage.JoinRoom) {
+    const payload = message.payload;
+    userManager.addUser(payload.name, payload.userId, payload.roomId, ws);
+  }
+
+  if (message.type === SupportedMessage.SendMessage) {
+    const payload = message.payload;
+    const user = userManager.getUser(payload.roomId, payload.userId);
+    if (!user) {
+      console.log("User not found!");
+      return;
+    }
+    let chat = store.addChats(
+      payload.userId,
+      user.name,
+      payload.roomId,
+      payload.message
+    );
+    if (!chat) {
+      return;
+    }
+
+    const outgoingPayload: OutgoingMessage = {
+      type: OutgoingSupportedMessages.AddChat,
+      payload: {
+        chatId: chat.id,
+        roomId: payload.roomId,
+        message: payload.message,
+        name: payload.message,
+        upvotes: 0,
+      },
+    };
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
+
+  if (message.type === SupportedMessage.UpvoteMessage) {
+    const payload = message.payload;
+    const chat = store.upVote(payload.userId, payload.roomId, payload.chatId);
+    console.log("Inside Upvote");
+    if (!chat) {
+      return;
+    }
+    console.log("inside uvote 2");
+
+    const outgoingPayload: OutgoingMessage = {
+      type: OutgoingSupportedMessages.UpdateChat,
+      payload: {
+        chatId: payload.chatId,
+        roomId: payload.roomId,
+        upvotes: chat.upvotes.length,
+      },
+    };
+
+    console.log("inside upvote 3");
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
+}
