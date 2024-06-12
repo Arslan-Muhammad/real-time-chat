48ef348460fdec917123ed6d3c3126f62b6a5e74
Arslan <muhammad.arslan.82jb@gmail.com>
2024-06-12 23:26:28 +0500

    tailwind added and configured and basic landing page developed

--- RTC/src/App.tsx
+++ RTC/src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-3 min-h-screen">
+        <div className="m-3 rounded border-dashed border-2 border-indigo-300 grid">
+          <div className="rounded border-dotted border-2 border-indigo-300 mx-auto mt-2 h-fit px-4 font-semibold text-2xl">
+            Chat
+          </div>
+        </div>
+        <div className="m-3 rounded border-dashed border-2 border-indigo-300">
+          <div className="rounded border-dotted border-2 border-indigo-300 mx-auto mt-2 h-fit px-4 font-semibold text-2xl">
+            Upvotes
+          </div>
+        </div>
+        <div className="m-3 rounded border-dashed border-2 border-indigo-300"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
